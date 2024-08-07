147f2a15987788abea03b3c22134bc30b910ac49
tanikazetacho <braulujos@outlook.com>
2024-08-06 19:43:55 -0600

    fix temperture by days (use reduce) & add styling conditional

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ module.exports = {
     extend: {
       colors: {
         'cold-blue': '#25AFFF',
-        'hot-red': '#bd1816'
+        'hot-red': '#bd1816',
+        'selected-green': '#7cb244'
       },
+      spacing: {
+        '5px': '5px',
+      }
     },
   },
   plugins: [],
